a2194c8d33aa2b7bac97896f3429e361206cddbb
Allan Muruha <allanmuruha@gmail.com>
2022-10-12 21:41:11 +0300

    Finish the basic game structure

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+let PENCOLOR = "#333333"
+let DRAWING = false;
+
+function drawGrid(e) {
+    if (e.type === "mouseover" && !DRAWING) return;
+    this.style.backgroundColor = PENCOLOR; 
+}
+
 function createGrid(size) {
-    container = document.querySelector("div.container");
+    container = document.querySelector("div.grid");
     container.textContent = "";
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     for (let i = 0; i < size**2; i++) {
         square = document.createElement("div");
         square.classList.add("grid-square");
+        square.addEventListener("mouseover", drawGrid);
+        square.addEventListener("mousedown", drawGrid);
         container.appendChild(square);
     }
+}
+
+function updateGrid() {
+    size = this.value;
+    label = document.querySelector('.settings label[for="grid"]');
+    label.innerText = `${size}×${size}`;
+    createGrid(size);
+}
+
+function updateColor() {
+    PENCOLOR = this.value;
+}
+
+function clearGrid() {
+    const SQUARES = document.querySelectorAll(".grid div.grid-square");
+    SQUARES.forEach(square => square.removeAttribute("style"));
 }
